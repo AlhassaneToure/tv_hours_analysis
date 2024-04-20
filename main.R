def482908c822125ee4012682f8e2f57518804cf
AlhassaneToure <161738768+AlhassaneToure@users.noreply.github.com>
2024-04-20 18:29:07 +0200

    Update main.R

---
+++ main.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(here)
+
+tv_hours_tables <- gss_cat %>% 
+  group_by(marital) %>% 
+  summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
+
+write_csv(tv_hours_tables, here("TV_hours_by_marital.csv"))
