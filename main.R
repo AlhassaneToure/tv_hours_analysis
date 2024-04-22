fab050a7ffbaa117724ee7f56c8d7a655eb8913c
AlhassaneToure <161738768+AlhassaneToure@users.noreply.github.com>
2024-04-22 02:16:38 +0200

    Shorten names

--- main.R
+++ main.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 library(here)
 
-tv_hours_tables <- gss_cat %>% 
+tv_hours_tab <- gss_cat %>% 
   filter(age < 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
-write_csv(tv_hours_tables, here("TV_hours_by_marital.csv"))
+write_csv(tv_hours_tab, here("TV_hours_by_marital.csv"))
